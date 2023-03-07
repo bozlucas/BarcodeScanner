04df66c47793cf57de3aa63f86fc747dc8f7fda7
Lucas <lucasprc93@gmail.com>
2023-03-07 11:59:43 -0800

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Lucas on 2023-03-07.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
